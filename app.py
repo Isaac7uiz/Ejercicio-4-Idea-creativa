db6f7d5d94dad066f4212d6f0cff0e8d23912658
Isaac7uiz <isaaktuiz@gmail.com>
2025-03-16 00:26:33 -0600

    segundo commit ejercicio

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        modelo = request.form['modelo']
+        serial = request.form['serial']
+        return redirect(url_for('confirmacion', nombre=nombre, modelo=modelo, serial=serial))
+    return render_template('registro.html')
+
+@app.route('/confirmacion')
+def confirmacion():
+    nombre = request.args.get('nombre')
+    modelo = request.args.get('modelo')
+    serial = request.args.get('serial')
+    return render_template('confirmacion.html', nombre=nombre, modelo=modelo, serial=serial)
+
+if __name__ == '__main__':
     app.run(debug=True)
